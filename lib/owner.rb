752ab545a1da2c780faea2a1aaa4119f711dc7fb
Lauren Dove <elledoveinc@gmail.com>
2019-10-12 00:15:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,75 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :cats
+  attr_reader :name, :species
+  @@all = [ ]
+  
+  def initialize(name)
+   @name = name
+  @species = species
+  @@all << self
+  end
+ 
+ def species
+   "human"
+ end
+ 
+ def say_species
+   "I am a human."
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def self.count
+   @@all.count
+   
+ end
+ 
+ 
+ def self.reset_all
+   @@all.clear
+ end
+  
+  def cats 
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+   #binding.pry
+  end
+  
+  def dogs 
+     Dog.all.select do |doggie|
+     doggie.owner == self 
+     end
+    
+   end
+   
+   #binding.pry
+   
+   
+   def buy_cat(name)
+    Cat.new(name, self)
+     
+  end
+  
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dog
+    self.Dog.each{|d| d.mood("happy")
+  end
+  
+  
+  
+  def pets 
+    pets = dog + cat
+  end
+  deg
+    
+  
 end
+
